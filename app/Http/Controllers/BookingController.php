0966e608ab85748c27cc7c892a245fe4e1a01816
Beka <bekatediashvili1995@gmail.com>
2023-10-27 00:48:19 +0400

    created BookingAsGuestController ,  Request , Resource added delete method in BookingController

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -16,7 +16,7 @@ class BookingController extends Controller
      */
     public function index()
     {
-         return Booking::all();
+        return Booking::all();
     }
 
     /**
@@ -37,7 +37,7 @@ class BookingController extends Controller
         $data = $request->validated();
         $user = auth()->user();
 
-        $studio= $course->studio;
+        $studio = $course->studio;
         $course = $studio->courses->where('studio_id', $studio->id)->first();
         if ($course) {
 
@@ -49,7 +49,7 @@ class BookingController extends Controller
 
             $booking->save();
 
-            return response()->json(['message' => 'Booking created successfully', 'data' => new BookingResource($booking) ], 201);
+            return response()->json(['message' => 'Booking created successfully', 'data' => new BookingResource($booking)], 201);
         } else {
             // User has not previously booked this class and is not authorized to book
             return response()->json(['message' => 'You are not authorized to book this class.'], 403);
@@ -86,6 +86,13 @@ class BookingController extends Controller
      */
     public function destroy(Booking $booking)
     {
-        //
+        if ($booking->user_id === auth()->user()->id) {
+
+            $booking->delete();
+            return response()->json(['message' => 'Booking deleted successfully']);
+        } else {
+            return response()->json(['message' => 'Unauthorized to delete this booking'], 403);
+        }
     }
+
 }
